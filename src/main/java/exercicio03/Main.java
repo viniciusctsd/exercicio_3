d632a1da5622a91b81c533caef448382d5feae6f
Vinicius Almeida <viniciusdealmeida@hotmail.com>
2021-03-28 16:06:35 -0300

    first commit

---
+++ src/main/java/exercicio03/Main.java
@@ -0,0 +1,33 @@
+package exercicio03;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Digite uma idade: ");
+		int idade = teclado.nextInt();
+		
+		
+		if (idade > 20) {
+			System.out.println("Idade maior que 20");
+		} else if( idade > 12 && idade < 17) {
+			System.out.println("Idade maior que 12 e menor que 17");
+		}else if (idade == 7 || idade < 5) {
+			System.out.println("Idade igual a 7 OU menor que 5");
+		}else {
+			System.out.println("Outras combinações");
+		}
+		
+	}
+
+}
